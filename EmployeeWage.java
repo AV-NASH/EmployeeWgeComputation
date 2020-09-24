13f5a9037268f164f36c8c75e2364baf8ebada76
AV-NASH <avinash21213434@gmail.com>
2020-09-24 18:19:18 +0530

    Employee absent/present feature done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,5 +4,15 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation");
+		
+		int employeePresent =1;
+		int value=(int)((Math.random()*10)%2);
+		if(value==employeePresent) {
+			System.out.println("Employee present");
+		}
+		
+		else {
+			System.out.println("Employee absent");
+		}
 	}
 }
