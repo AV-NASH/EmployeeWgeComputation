7f108f667bcfe33f88c11c6f586f5118ec2f8ca9
AV-NASH <avinash21213434@gmail.com>
2020-09-24 21:12:32 +0530

    Employee daily wage feature done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,18 +1,23 @@
 package employeeWagePackage;
 
 public class EmployeeWage {
+public static int wagePerHour=20;
+
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation");
-		
+		int fullDayHour;
 		int employeePresent =1;
 		int value=(int)((Math.random()*10)%2);
 		if(value==employeePresent) {
-			System.out.println("Employee present");
+			fullDayHour=8;	
+		}
+
+		else {
+				fullDayHour=0;
 		}
 		
-		else {
-			System.out.println("Employee absent");
-		}
-	}
+		int wage=wagePerHour*fullDayHour;
+		System.out.println("wage of the employee is"+wage);
+	} 
 }
