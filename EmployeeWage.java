75e7d9845c2d5f96ab46dc30b2ec23c976746217
AV-NASH <avinash21213434@gmail.com>
2020-09-26 17:08:28 +0530

    Refactoring the code to  implement OOPs concept

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,48 +1,54 @@
 package employeeWagePackage;
 
 public class EmployeeWage {
-public static final int wagePerHour=20;
-public static final int noofWorkingDays=20;
-public static final int noOfMaxHours=100;
 
+	public final int wagePerHour=20;
+	 public final int noofWorkingDays=20;
+	 public final int noOfMaxHours=100; 
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation");
-		int fullDayHour;
-		final int fullTime=1;
-		final int partTime=2;
-		int EmpWage=0;
-		int noofdays=0;
-		int noofhours=0;
-		
-		while (noofdays<noofWorkingDays && noofhours<noOfMaxHours) {
-			
-		
-			int value=(int)((Math.random()*10)%3);
-			
-			switch (value)
-			{
-			case fullTime: {
-						fullDayHour=8;
-						break;
-			}
-			case partTime: {
-						fullDayHour=4;
-						break;
-			}
-			
-			default: {
-						fullDayHour=0;
-						break;
-					}
-			}
-			
-			noofhours=noofhours+fullDayHour;
-			noofdays=noofdays+1;
-			int wage=wagePerHour*fullDayHour;
-			 EmpWage=EmpWage+wage;
-		}
-		
-		System.out.println("wage of the employee is"+EmpWage);
+		EmployeeWage emp=new EmployeeWage();
+		emp.calcSalary();	
 	} 
+
+	public void calcSalary () {
+		
+		  	int fullDayHour;
+			final int fullTime=1;
+			final int partTime=2;
+			int EmpWage=0;
+			int noofdays=0;
+			int noofhours=0;
+			
+			while (noofdays<noofWorkingDays && noofhours<noOfMaxHours) {
+				
+				int value=(int)((Math.random()*10)%3);
+				
+				switch (value)
+				{
+				case fullTime: {
+							fullDayHour=8;
+							break;
+				}
+				case partTime: {
+							fullDayHour=4;
+							break;
+				}
+				
+				default: {
+							fullDayHour=0;
+							break;
+						}
+				}
+				
+				noofhours=noofhours+fullDayHour;
+				noofdays=noofdays+1;
+				int wage=wagePerHour*fullDayHour;
+				 EmpWage=EmpWage+wage;
+			}
+			
+			System.out.println("wage of the employee is "+EmpWage);
+	}
+	
 }
